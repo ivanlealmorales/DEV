f04d58b39bad1edf50d3f48854d7ad527c9cef17
ivanlealmorales <prof_ivanmorales@hotmail.com>
2023-08-22 07:15:04 -0300

    Aula

--- JS/teste.js
+++ JS/teste.js
@@ -1,1 +1,15 @@
-console.log("Olá, mundo!");
+function StringUtil() {
+    this.inverter = function(texto) {
+      return texto.split('').reverse().join('');
+    };
+  
+    this.contarCaracteres = function(texto) {
+      return texto.length;
+    };
+  }
+  
+  const utilString = new StringUtil();
+  
+  console.log(utilString.inverter("Olá")); // Saída: álO
+  console.log(utilString.contarCaracteres("Hello, world!")); // Saída: 13
+  
